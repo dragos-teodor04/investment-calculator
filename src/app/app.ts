6547f0867d8a1000f52d319c0948813715545e66
Petre Dragos Teodor <petredragosteodor@gmail.com>
2025-12-17 19:17:45 +0200

    Added user-input Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { UserInput } from './user-input/user-input';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, UserInput],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('investment-calculator');
