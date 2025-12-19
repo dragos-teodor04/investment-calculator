8f415a684a554e7b34cc845219e2a309482bc84a
Petre Dragos Teodor <petredragosteodor@gmail.com>
2025-12-19 18:29:31 +0200

    Added formatting for investment results

--- src/app/investment-results/investment-results.ts
+++ src/app/investment-results/investment-results.ts
@@ -1,8 +1,8 @@
 import { Component, Input, input } from '@angular/core';
-
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.html',
   styleUrl: './investment-results.css',
 })
